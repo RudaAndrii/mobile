312a67fcfee467af5c88c36133460acef94a434f
Andrii Ruda <andrii.ruda@botscrew.com>
2020-10-25 16:40:08 +0200

    Removed unnecessary code

--- mobile/mobileApp.swift
+++ mobile/mobileApp.swift
@@ -9,12 +9,13 @@ import SwiftUI
 
 @main
 struct mobileApp: App {
-    let persistenceController = PersistenceController.shared
-
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
+    }
+    
+    init() {
+        print("Hello, World!")
     }
 }
